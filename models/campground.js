cea890932e30f45f976541d28602a244a152a250
Abderaouf Yettou <abderaouf.yettou@gmail.com>
2021-01-01 23:11:36 +0100

    add mongoose middleware for deletecampground

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const Review = require('./review');
 
 const { Schema } = mongoose;
 
@@ -9,6 +10,11 @@ const campSchema = new Schema({
   description: String,
   location: String,
   reviews: [{ type: Schema.Types.ObjectId, ref: 'Review' }],
+});
+campSchema.post('findOneAndDelete', async function (doc) {
+  if (doc) {
+    await Review.deleteMany({ _id: { $in: doc.reviews } });
+  }
 });
 
 const Campground = mongoose.model('Campground', campSchema);
